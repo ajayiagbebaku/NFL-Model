d8d7856680635b0ba752a21b0f574404f8f81369
ajayi1987 <ajayi1987@me.com>
2021-11-11 09:41:35 -0600

    Update nflmodel.py

--- nflmodel.py
+++ nflmodel.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy
 st.set_page_config(page_title="NFL Projections 2021",layout="wide")
 st.header("Week 10 Projections: Updated 11/11/2021.")
-st.text("Lines Vegas Lines updated Sunday morning")
+st.text("Lines vegas lines updated Sunday morning")
 st.text("Spread Win/Loss: (4-9).308")
 st.text("Over/Under Win Loss: (6-7).462")
 df = pd.read_excel("UpdatedResultsNFL.xlsx")
