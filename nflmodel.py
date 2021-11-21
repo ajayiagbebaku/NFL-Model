b833dabcd68aa05607b557a343aed7c9d23e350b
ajayi1987 <ajayi1987@me.com>
2021-11-20 23:59:08 -0600

    added PACE calculation. Updated Proj

--- nflmodel.py
+++ nflmodel.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy
 st.set_page_config(page_title="NFL Projections 2021",layout="wide")
-st.header("Week 11 Projections: Updated 11/17/2021.")
+st.header("Week 11 Projections: Updated 11/20/2021.")
 st.text("Vegas lines updated Sunday morning")
 st.text("Spread Win/Loss: (10-17).37")
 st.text("Over/Under Win Loss: (13-14).481")
