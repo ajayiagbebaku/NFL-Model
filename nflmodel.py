0e5e909cbd301eb1b1e1473db68b48821df0cdde
ajayi1987 <ajayi1987@me.com>
2021-12-17 23:57:47 -0600

    Update nflmodel.py

--- nflmodel.py
+++ nflmodel.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy
 st.set_page_config(page_title="NFL Projections 2021",layout="wide")
-st.header("Week 13 Projections: Updated 12/16/2021.")
+st.header("Week 15 Projections: Updated 12/16/2021.")
 st.text("Final Projections are on Sunday morning")
 st.text("Spread Win/Loss: (44-40).524")
 st.text("Over/Under Win Loss: (41-44).482")
